e6c31c7ead6c6704482d6711b8ed9624c53251e7
Maxime Bourget <bmx007@gmail.com>
2026-04-03 19:04:00 +0100

    With operators

--- src/Data/NoDF/Wector.hs
+++ src/Data/NoDF/Wector.hs
@@ -12,13 +12,58 @@ import qualified Data.Vector.Mutable.Sized as MS -- (Vector)
 import Control.Monad.ST
 
 
+-- * Operations on permutation
+--
+infixl 5 @>, @>$, @>=, @>~
+(@>) :: Vector n (Finite m) -> Vector m a -> Vector n a
+p @> v = fmap (index v) p
+
+(@>$) :: Functor f => Vector n (f (Finite m)) -> Vector m a -> Vector n (f a)
+p @>$ v = fmap (fmap (index v)) p
+
+(@>=) :: Monad f => Vector n (f (Finite m)) -> Vector m (f a) -> Vector n (f a)
+p @>= v = fmap ((index v) =<<) p
+
+(@>~) :: (Traversable t , Monad f)  => Vector n (t (Finite m)) -> Vector m (f a ) -> Vector n (f (t a))
+p @>~ v = sequence <$> p @>$ v
+
+
 -- | A "double" vector with a shared spine
 data Wector n grp a =
             Wector { windex  :: Vector n (Finite grp)
-                   , wspine :: Vector grp a
+                   , witems :: Vector grp a
                    }
      deriving (Show, Eq, Functor, Foldable, Traversable)
      
+wbroadcast :: Wector n grp a -> Vector n a
+wbroadcast w = windex w @> witems w
+
+expandW :: Functor f => Wector n grp (f (Finite n)) -> Wector n grp (f (Finite grp))
+expandW w = w { witems = witems w @>$ windex w }
+
+wexpand :: Functor f => Wector n grp (f (Finite n)) -> Vector grp (f (Finite grp))
+wexpand = witems . expandW
+
+-- | when witems a <= witems ab
+composeW :: Monad f => Wector a ab (f (Finite a)) -> Wector ab abcd (f (Finite ab)) -> Wector a abcd (f (Finite a))
+composeW a ab = Wector ( windex a @> windex ab)
+                      ( witems ab @>= witems a)
+                      
+
+inverseW :: Wector n grp (Finite n) -> Wector grp n (Finite grp)
+inverseW w = Wector ( witems w )
+                    ( windex w)
+                    
+inverseWM :: Monad m => Wector n grp (m (Finite n)) -> m (Wector grp n (Finite grp))
+inverseWM w = fmap inverseW $ sequence w 
+
+-- | op can be @> or @>$
+broadcastWith :: Monad f => (Vector grp a -> Vector m (Finite grp) -> Vector g2 b) -> Wector n grp a -> Wector m grp (f (Finite g2)) -> f ( Wector n g2 b )
+broadcastWith op a b = fmap go $ sequence (witems b) where
+            go itemsb = Wector ( windex a @> itemsb)
+                           ( witems a `op` windex b)
+
+     {-
 infixl 5 @>, <@, @$>, <$@
 (@>) :: Vector n (Finite m) -> Vector m a -> Vector n a
 p @> v = fmap (index v) p
@@ -71,6 +116,8 @@ xx va vb = fmap ((index (vb)) =<<) va
 xxf f va vb = fmap ((f $ index (vb)))  va
 
 
+-}
+
 selecting ::  forall n r . KnownNat n => Vector n Bool -> (forall s . KnownNat s => Wector s n (Maybe (Finite s)) ->  r ) -> r
 selecting v f = let 
    selection = Unsized.filter (\fi -> v `index` fi )
@@ -97,6 +144,17 @@ taking n v f = let
            in f $ Wector sel back
 
 
+selectingWith  :: forall n a r . KnownNat n => (Unsized.Vector (Finite n) -> Unsized.Vector (Finite n)) -> Vector n a -> (forall sel . KnownNat sel => Wector sel n (Maybe (Finite sel)) -> r) -> r
+selectingWith select v f = let
+   selection = select $ fromSized 
+                 $ S.generate id
+   in case selection of
+        SomeSized sel -> let 
+           back = runST $ do
+                        mv <- MS.replicate Nothing
+                        S.imapM_ (\is i -> MS.write mv i (Just is)) sel
+                        S.freeze mv
+           in f $ Wector sel back
 
 
 main :: IO ()
@@ -109,12 +167,9 @@ main = do
                   (s2 :: Wector s2 s1 (Maybe (Finite s2))) -> do
                        print ("S1", s1)
                        print ("S2", s2)
+                       
+                       let s2' = composeW s2 s1
+                       print $ windex s2' @> q
+                       print $ wexpand s2'  @>$ v'q
 
-                       let x = windex s2 @> windex s1 :: Vector s2 (Finite n)
-                           y = xxf (=<<) (wspine s1) (wspine s2) :: Vector n (Maybe (Finite s2))
-                           z = y @$> x :: Vector n (Maybe (Finite n))
-                       print x
-                       print y
-                       print z
-                       print $ z @$> v
         
